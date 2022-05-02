1880bb1141a9a1d59d0ac7c528598250170ec594
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2022-05-02 10:08:48 +0200

    Init Project

---
+++ Engine/src/Entry.cpp
@@ -0,0 +1,17 @@
+//Copyright(C) 2022 Chris Husky <chrishuskywolf@gmail.com>
+//
+//This file is part of the Copper Engine project.
+//
+//The Copper Engine project can not be copied and /or distributed without the express
+//permission of Chris Husky <chrishuskywolf@gmail.com>.
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello, World!" << std::endl;
+	std::cin.get();
+
+	return 0;
+
+}
